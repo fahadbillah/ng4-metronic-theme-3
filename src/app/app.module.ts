c7b823d769c9a4876629fc933741123cc505b0e6
Fahad Billah <fahadbillah@yahoo.com>
2017-10-31 18:29:46 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
+import { SideNavBarComponent } from './components/side-nav-bar/side-nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
+import { PageSubHeaderComponent } from './components/page-sub-header/page-sub-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    SideNavBarComponent,
+    FooterComponent,
+    PageContentComponent,
+    PageSubHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/page-sub-header/page-sub-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-sub-header',
+  templateUrl: './page-sub-header.component.html',
+  styleUrls: ['./page-sub-header.component.css']
+})
+export class PageSubHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
